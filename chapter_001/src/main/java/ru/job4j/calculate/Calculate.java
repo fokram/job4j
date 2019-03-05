4eebac7e44b572c455c6af0776d236ba192470e5
Roman Markov <rsm05@inbox.ru>
2019-03-05 12:03:54 +0300

    1.3. JavaDoc. Code convention[#116648]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,4 +1,10 @@
 package ru.job4j.calculate;
+/**
+*Calculate Класс вывода hello woirld
+*@author rMarkov
+*@since 05.03.2019
+*@version 1
+*/
 public class Calculate
 {
 	public static void main(String[] args) {
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Roman Markov (rsm05@inbox.ru)
+* @version 1
+* @since 05.03.2019
+*/
+package ru.job4j.calculate;
