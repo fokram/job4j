9a34f9644172cbd29b73f5657c3f1433b7f64cf7
Roman Markov <rsm05@inbox.ru>
2019-03-06 09:31:38 +0300

    1.3.1 Maven Plugins #116645

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -8,10 +8,8 @@ package ru.job4j.calculate;
 public class Calculate
 {
 /**
-*основной метод
-*@author rMarkov
-*@since 05.03.2019
-*@version 1
+* Method Main
+* @param args params.
 */
 	public static void main(String[] args) {
 		System.out.println("Hello world");
