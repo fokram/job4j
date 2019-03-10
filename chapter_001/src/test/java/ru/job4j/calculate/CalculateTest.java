f1c6f93a60b71b5788c0608411f44b20e449af52
roman.markov <rsm05@inbox.ru>
2019-03-10 10:57:09 +0300

    1.4. JUnit[#116649]

---
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -0,0 +1,23 @@
+﻿package ru.job4j.calculate;
+
+/**
+ * Test.
+ *
+ * @author Roman.Markov
+ * @version 1
+ * @since 08.03.2019
+ */
+ public class CalculateTest {
+  /**
+ * Test echo.
+ */
+
+	@Test
+	 public void whenTakeNameThenThreeEchoPlusName() {
+	    String input = "qwerty";
+	    String expect = "Echo, echo, echo : qwerty"; 
+	    Calculate calc = new Calculate();
+	    String result = calc.echo(input);
+	    assertThat(result, is(expect));
+	 }
+}
