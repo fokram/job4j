0b3ad3c898cc41dfedbf47e147a109f16ece2441
Roman Markov <rsm05@inbox.ru>
2019-03-17 00:10:11 +0300

    3.1. Элементарный калькулятор.[#116639]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -6,40 +6,40 @@ package ru.job4j.calculator;
 *@version 1
 */
 public class Calculator {
-/**
- * Method add
- * @param first слагаемое
- * @param second слагаемое
- * @return double сумма
- */
-	public double add (double first, double second) {
-		return first+second;
-	}
-/**
- * Method subtract
- * @param first уменьшаемое
- * @param second вычитаемое
- * @return double разница
- */
-	public double subtract (double first, double second) {
-		return first-second;
-	}
-/**
- * Method div
- * @param first делимое
- * @param second делитель
- * @return double частное
- */
-	public double div (double first, double second) {
-		return first/second;
-	}
-/**
- * Method multiple
- * @param first множитель
- * @param second множитель
- * @return double произведение
- */
-	public double multiple (double first, double second) {
-		return first*second;
-	}
+  /**
+   * Method add
+   * @param first слагаемое
+   * @param second слагаемое
+   * @return double сумма
+   */
+  public double add (double first, double second) {
+  	return first+second;
+  }
+  /**
+   * Method subtract
+   * @param first уменьшаемое
+   * @param second вычитаемое
+   * @return double разница
+   */
+  public double subtract (double first, double second) {
+  	return first-second;
+  }
+  /**
+   * Method div
+   * @param first делимое
+   * @param second делитель
+   * @return double частное
+   */
+  public double div (double first, double second) {
+  	return first/second;
+  }
+  /**
+   * Method multiple
+   * @param first множитель
+   * @param second множитель
+   * @return double произведение
+   */
+  public double multiple (double first, double second) {
+  	return first*second;
+  }
 }
