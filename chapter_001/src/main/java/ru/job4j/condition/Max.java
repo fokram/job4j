fc287a04c57fe27a0793fa0add7fbcc47605585f
Roman Markov <rsm05@inbox.ru>
2019-03-20 22:39:56 +0300

    4.2. Максимум из двух чисел[#116612]

---
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -0,0 +1,14 @@
+package ru.job4j.condition;
+/**
+ *condition Класс определения расстояния между двуми точками на плоскости
+ *@author rMarkov
+ *@since 17.03.2019
+ *@version 1
+ */
+
+public class Max {
+  public int max(int left, int right) {
+    int result = right > left ? right : left;
+    return result;
+  }
+}
