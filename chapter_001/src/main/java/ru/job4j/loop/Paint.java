5491b61386c0bc1908021c377e29d6fea0675cac
Roman Markov <rsm05@inbox.ru>
2019-03-24 18:11:03 +0300

    5.5. Рефакторинг кода.[#116617]

--- chapter_001/src/main/java/ru/job4j/loop/Paint.java
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -1,4 +1,7 @@
 package ru.job4j.loop;
+
+import java.util.function.BiPredicate;
+
 /**
  * Paint Класс рисование
  * @author RomanM
@@ -8,74 +11,58 @@ package ru.job4j.loop;
 public class Paint {
 
   /**
-   * piramid метод рисования пирамиды целиком
-   * @param h - высота пирамиды
+   * loopBy метод рисования пирамиды целиком
+   * @param height - высота пирамиды
+   * @param widht - высота пирамиды
+   * @param predict - условие выражения
    * @author RomanM
-   * @version 1
-   * @since 23.03.2019
+   * @since 24.03.2019
    */
-  public String piramid(int h) {
+  private String loopBy(int height, int widht, BiPredicate<Integer, Integer> predict) {
     StringBuilder screen = new StringBuilder();
-    String ln = System.lineSeparator();
-    for (int y = 1; y <= h; y++) {
-      for (int x = 1; x <= 2 * h - 1; x++) {
-        if (x >= h - y + 1 && x <= h + y - 1) {
+    for (int row = 0; row != height; row++) {
+      for (int column = 0; column != widht; column++) {
+        if (predict.test(row, column)) {
           screen.append("^");
         } else {
           screen.append(" ");
         }
       }
-      // добавляем перевод на новую строку.
-      screen.append(ln);
+      screen.append(System.lineSeparator());
     }
     return screen.toString();
+  }
+  /**
+   * piramid метод рисования пирамиды целиком
+   * @param height - высота пирамиды
+   * @author RomanM
+   * @since 23.03.2019
+   */
+  public String piramid(int height) {
+    return this.loopBy(
+            height,
+            2 * height - 1,
+            (row, column) -> row >= height - column - 1 && row + height - 1 >= column
+    );
   }
 
   /**
    * rightTrl метод рисования правой стороны пирамиды
-   * @param h - высота пирамиды
+   * @param height - высота пирамиды
    * @author RomanM
-   * @version 1
    * @since 24.03.2019
    */
-  public String rightTrl(int h) {
-    StringBuilder screen = new StringBuilder();
-    String ln = System.lineSeparator();
-    for (int y = 1; y <= h; y++) {
-      for (int x = 1; x <= h; x++) {
-        if (x <= y) {
-          screen.append("^");
-        } else {
-          screen.append(" ");
-        }
-      }
-      // добавляем перевод на новую строку.
-      screen.append(ln);
-    }
-    return screen.toString();
+  public String rightTrl(int height) {
+    return this.loopBy(height, height, (row, column) -> row >= column);
   }
 
   /**
    * leftTrl метод рисования левой стороны пирамиды
-   * @param h - высота пирамиды
+   * @param height - высота пирамиды
    * @author RomanM
-   * @version 1
    * @since 24.03.2019
    */
-  public String leftTrl(int h) {
-    StringBuilder screen = new StringBuilder();
-    String ln = System.lineSeparator();
-    for (int y = 1; y <= h; y++) {
-      for (int x = 1; x <= h; x++) {
-        if (x > h - y) {
-          screen.append("^");
-        } else {
-          screen.append(" ");
-        }
-      }
-      // добавляем перевод на новую строку.
-      screen.append(ln);
-    }
-    return screen.toString();
+  public String leftTrl(int height) {
+    return this.loopBy(height, height, (row, column) -> row >= height - column - 1);
   }
 }
