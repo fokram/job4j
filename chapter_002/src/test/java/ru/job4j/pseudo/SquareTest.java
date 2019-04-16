cf322d12893ea593d674bce481e6394400b6aa83
Roman Markov <rsm05@inbox.ru>
2019-04-16 15:04:20 +0300

    4. Используя шаблон проектирования - стратегию[#116688]

---
+++ chapter_002/src/test/java/ru/job4j/pseudo/SquareTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.pseudo;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author ROman Markov
+ * @version $Id$
+ * @since 16.04.2019
+ */
+public class SquareTest {
+    @Test
+    public void whenDrawSquare() {
+        Square square = new Square();
+        assertThat(
+                square.draw(),
+                is(
+                        new StringBuilder()
+                                .append("+++++++\n")
+                                .append("+     +\n")
+                                .append("+     +\n")
+                                .append("+++++++")
+                                .toString()
+                )
+        );
+    }
+}
