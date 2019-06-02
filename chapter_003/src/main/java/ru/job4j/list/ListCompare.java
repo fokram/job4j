0dbf3dbd950c94077447ebb450cb317caab4372f
Roman Markov <rsm05@inbox.ru>
2019-06-02 11:51:42 +0300

    3. Компаратор для строк.[#116662]

--- chapter_003/src/main/java/ru/job4j/list/ListCompare.java
+++ chapter_003/src/main/java/ru/job4j/list/ListCompare.java
@@ -6,18 +6,15 @@ public class ListCompare implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
         int result = 0;
-        for (int i = 0; i < left.length(); i++) {
+        for (int i = 0; i < left.length() && result == 0; i++) {
             if (right.length() <= i) {
                 result = -1;
                 break;
             }
             result = Character.compare(left.charAt(i), right.charAt(i));
-            if (result != 0) {
-                break;
-            }
         }
-        if (result == 0 && right.length() > left.length()) {
-            result = -1;
+        if (result == 0) {
+            result = left.length() - right.length();
         }
         return result;
     }
