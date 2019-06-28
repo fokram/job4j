fbc3c24342ec3cc666f2df5e4e5308866766c544
Roman Markov <rsm05@inbox.ru>
2019-06-28 13:24:16 +0300

    4. Преобразование List в Map.[#116578]

--- chapter_004/src/main/java/ru/job4j/school/Student.java
+++ chapter_004/src/main/java/ru/job4j/school/Student.java
@@ -2,12 +2,31 @@ package ru.job4j.school;
 
 public class Student {
     private int score;
+    private String firstName;
+    private String lastName;
 
-    public Student(int score) {
+    public Student(String firstName, String lastName, int score) {
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.score = score;
     }
 
     public int getScore() {
         return score;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{"
+                + "score=" + score
+                + ", firstName='" + firstName + '\''
+                + ", lastName='" + lastName + '\''
+                + '}';
     }
 }
--- chapter_004/src/test/java/ru/job4j/school/SchoolTest.java
+++ chapter_004/src/test/java/ru/job4j/school/SchoolTest.java
@@ -3,7 +3,7 @@ package ru.job4j.school;
 import org.junit.Test;
 
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Map;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -12,24 +12,23 @@ public class SchoolTest {
     @Test
     public void getClassesList() {
         List<Student> students = List.of(
-                new Student(5),
-                new Student(33),
-                new Student(80),
-                new Student(100),
-                new Student(90),
-                new Student(50),
-                new Student(24),
-                new Student(65),
-                new Student(22),
-                new Student(97),
-                new Student(86),
-                new Student(1),
-                new Student(56),
-                new Student(80));
-        List<Student> a = new School().collect(students, student -> student.getScore() <= 50);
-        List<Student> b = new School().collect(students, student -> student.getScore() > 50 && student.getScore() <= 70);
-        List<Student> c = new School().collect(students, student -> student.getScore() > 70);
-
+                new Student("Andrew", "Belyakov", 5),
+                new Student("John", "Belkovsky", 33),
+                new Student("Ivan", "Ivanov", 80),
+                new Student("Dmitry", "Balabanov", 100),
+                new Student("Sergey", "Vanin", 90),
+                new Student("Mark", "Frankenstein", 50),
+                new Student("Arseniy", "Belov", 24),
+                new Student("Boris", "Petrov", 65),
+                new Student("Leo", "Smirnov", 22),
+                new Student("Petr", "Petrov", 97),
+                new Student("Albert", "Qwert", 86),
+                new Student("Bengamin", "OIUytr", 1),
+                new Student("Robert", "Kjhgfd", 56),
+                new Student("Valentin", "Hbnm", 80));
+        Map<String, Student> a = new School().collect(students, student -> student.getScore() <= 50);
+        Map<String, Student> b = new School().collect(students, student -> student.getScore() > 50 && student.getScore() <= 70);
+        Map<String, Student> c = new School().collect(students, student -> student.getScore() > 70);
         boolean result = false;
         if (a.size() == 6 & b.size() == 2 & c.size() == 6) {
             result = true;
