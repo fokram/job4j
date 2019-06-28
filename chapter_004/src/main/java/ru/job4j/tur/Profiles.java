955231b47310491eb5a2dcab118060c0435c452b
Roman Markov <rsm05@inbox.ru>
2019-06-28 11:27:29 +0300

    3. Уникальность элементов и сортировка.[#116577]

--- chapter_004/src/main/java/ru/job4j/tur/Profiles.java
+++ chapter_004/src/main/java/ru/job4j/tur/Profiles.java
@@ -1,10 +1,16 @@
 package ru.job4j.tur;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Profiles {
     List<Address> collect(List<Profile> profiles) {
-        return profiles.stream().map(profile -> profile.getAddress()).collect(Collectors.toList());
+        return profiles
+                .stream()
+                .map(Profile::getAddress)
+                .distinct()
+                .sorted(Comparator.comparing(Address::getCity))
+                .collect(Collectors.toList());
     }
 }
--- chapter_004/src/test/java/ru/job4j/tur/ProfilesTest.java
+++ chapter_004/src/test/java/ru/job4j/tur/ProfilesTest.java
@@ -8,10 +8,12 @@ public class ProfilesTest {
     @Test
     public void getClassesList() {
         List<Profile> profiles = List.of(
-                new Profile(new Address("Moscow", "Lyubanka", 1, 1)),
-                new Profile(new Address("St.Peterburg", "Nevskiy", 2, 35)),
-                new Profile(new Address("Novgorod", "fghjm", 5, 111)),
-                new Profile(new Address("Ekaterinburg", ";lokj", 2, 35))
+                new Profile(new Address("Moscow", "Alyabina", 1, 1)),
+                new Profile(new Address("St.Peterburg", "Beef", 2, 35)),
+                new Profile(new Address("Novgorod", "Camel", 5, 111)),
+                new Profile(new Address("Ekaterinburg", "Dock", 2, 35)),
+                new Profile(new Address("Ekaterinburg", "Dock", 2, 35)),
+                new Profile(new Address("Abakan", "Jacks", 2, 35))
         );
         (new Profiles().collect(profiles)).forEach(System.out::println);
     }
