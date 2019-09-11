cd6c4c883fa37608104e9c117f9e1afb9eaf0c78
Roman Markov <rsm05@inbox.ru>
2019-09-11 10:55:35 +0300

    5.3.4. Задан связанный список. Определить цикличность.[#171383]

--- chapter_005/src/test/java/ru/job4j/generic/SimpleArrayListTest.java
+++ chapter_005/src/test/java/ru/job4j/generic/SimpleArrayListTest.java
@@ -2,6 +2,9 @@ package ru.job4j.generic;
 
 import org.junit.Test;
 import org.junit.Before;
+
+import java.util.Iterator;
+
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -44,5 +47,53 @@ public class SimpleArrayListTest {
         assertThat(list.delete(1), is(2));
         list.delete(1);
         assertThat(list.getSize(), is(1));
+    }
+
+    @Test
+    public void whenCycleThenFoundIt() {
+        class Node<E> {
+            private E data;
+            private Node<E> next;
+
+            Node(E data) {
+                this.data = data;
+            }
+
+            public boolean hasCycle(Node first) {
+                boolean isCycle = false;
+                DynamicList<Node> dl = new DynamicList();
+                Node currentChecked = first;
+                while (currentChecked.next != null
+                        && !isCycle) {
+                    Iterator it = dl.iterator();
+                    while (it.hasNext()) {
+                        if (it.next() == currentChecked) {
+                            isCycle = true;
+                            break;
+                        }
+                    }
+                    if (!isCycle) {
+                        dl.add(currentChecked);
+                        currentChecked = currentChecked.next;
+                    }
+                }
+                return isCycle;
+            }
+        }
+
+        Node first = new Node(1);
+        Node second = new Node(2);
+        Node third = new Node(3);
+        Node fourth = new Node(4);
+
+        first.next = second;
+        second.next = third;
+        third.next = fourth;
+        fourth.next = null;
+        assertThat(first.hasCycle(first), is(false));
+        fourth.next = second;
+        assertThat(first.hasCycle(first), is(true));
+        third.next = second;
+        assertThat(first.hasCycle(first), is(true));
     }
 }
