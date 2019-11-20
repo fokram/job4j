280d978f160c54edf9927ee20ba227e9a70724c4
Roman Markov <rsm05@inbox.ru>
2019-11-20 20:25:36 +0300

    2. Анализ доступности сервера.[#171416]

---
+++ chapter_005/src/main/java/ru/job4j/io/Analizy.java
@@ -0,0 +1,68 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Analizy {
+
+    private List badStatus = List.of("400", "500");
+    private List goodStatus = List.of("200", "300");
+
+    public void unavailable(String source, String target) {
+        List<String> listFile = read(source);
+        String problemAppear = "";
+        FileWriter wr = null;
+
+        try {
+            wr = new FileWriter(target);
+            for (String line: listFile) {
+                String[] str = line.split(" ");
+                if (str.length == 2) {
+                    if (badStatus.contains(str[0]) && problemAppear.isEmpty()) {
+                        problemAppear = str[1];
+                    } else if (!problemAppear.isEmpty() && goodStatus.contains((str[0]))) {
+                        wr.append(problemAppear + ";" + str[1] + "\n");
+                        problemAppear = "";
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                wr.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+//        while ((line = buffer.readLine()) != null) {
+//            String[] str = line.split(" ");
+//            if (str.length == 2) {
+//
+//            }
+//        }
+    }
+
+    private List<String> read(String path) {
+        List<String> result = new ArrayList<>();
+        try (BufferedReader buffer = new BufferedReader(new FileReader(path))) {
+            result = buffer.lines().collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        try (FileWriter fw = new FileWriter("/Users/r.markov/projects/job4j/unavailable.csv")) {
+            fw.append("15:01:30;15:02:32");
+            fw.append("15:10:30;23:12:32");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ chapter_005/src/test/java/ru/job4j/io/AnalizyTest.java
@@ -0,0 +1,62 @@
+package ru.job4j.io;
+
+import org.junit.Test;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class AnalizyTest {
+
+    @Test
+    public void whenAnalizeThenGetOutFile() {
+        String src = "/Users/r.markov/projects/job4j/log.csv";
+        String trg = "/Users/r.markov/projects/job4j/unavailable.csv";
+
+        Analizy a = new Analizy();
+        FileWriter wr = null;
+        try {
+            wr = new FileWriter(src);
+            wr.write("200 10:56:01\n" +
+                    "500 10:57:01\n" +
+                    "400 10:58:01\n" +
+                    "200 10:59:01\n" +
+                    "500 11:01:02\n" +
+                    "200 11:02:02");
+            wr.close();
+            a.unavailable(src, trg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                wr.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        BufferedReader logFile = null;
+        try {
+            logFile = new BufferedReader(new FileReader(trg));
+            List<String> logList = logFile.lines().collect(Collectors.toList());
+            assertThat(logList.get(0), is("10:57:01;10:59:01"));
+            assertThat(logList.get(1), is("11:01:02;11:02:02"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                logFile.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+}
